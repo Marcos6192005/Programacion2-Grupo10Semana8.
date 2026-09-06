466fa158651c4b9fdd88f0e42a3ddb32cab8ee89
kyroxs <mrchad356@gmail.com>
2026-09-06 11:13:00 -0600

    refactor(ui): Change app exit message v7

--- src/main/java/com/sv/grupo10/tienda/ui/Main.java
+++ src/main/java/com/sv/grupo10/tienda/ui/Main.java
@@ -131,7 +131,7 @@ public class Main {
                         break;
 
                     case 6:
-                        System.out.println("\nAbandonando Aplicacion...V5");
+                        System.out.println("\nAbandonando Aplicacion...V7");
                         break;
 
                     default:
